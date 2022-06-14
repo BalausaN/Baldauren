090a1fabe347abe1f96563c7219d8b617efadc6c
BalausaN <107469022+BalausaN@users.noreply.github.com>
2022-06-14 16:11:10 +0600

    Add files via upload

---
+++ sadik.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $(".news__slider-inner").slick({
+      nextArrow: 'button type="button" class="slick-btn slick-next"><button>',
+      prevArrow: 'button type="button" class="slick-btn slick-next"><button>',
+      Infinite: false
+    });
+})
+
+$('select').styler();
